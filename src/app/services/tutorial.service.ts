ff118a0a45ff46f01e7f359e09a451cb6e0641a1
Akash Sarkar <sarkarak.1994@gmail.com>
2020-10-01 16:53:30 +0530

    url changed

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
- const baseUrl = 'https://spring-boot-data-jpa-liberty-spring-boot-data-jpa-liberty-dev.clusteronedeliver-6e997a67873d748bc926b3848f2412ed-0000.us-east.containers.appdomain.cloud/tutorials';
+ const baseUrl = 'https://library-app-backend-wcare-ns.cloudpakforappsonedeliv-9d87abc9b6f9221a213bca93f0664811-0000.us-south.containers.appdomain.cloud/tutorials';
 //const baseUrl = 'http://' + window.location.hostname + ':8083/tutorials';
 @Injectable({
   providedIn: 'root'
